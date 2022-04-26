5026f6b34c8c50d136a8de3f8c615050f3569183
Yassir560 <hoossanbukshyassir@gmail.com>
2022-04-26 07:22:17 +0400

    Updating to current stuff
    Pushing the stuff I've worked on before creating the repo; I plan on getting to the fancy stuff right about nowmorrowsterday

---
+++ class.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int class_size = 5; const int name_limit = 16;
+
+
+struct classroom {
+    string fName, lName, fullName;
+} classA[ class_size ];
+
+string name_prompt( int count );
+
+void fillClassroom( int up_to = class_size) ; void dispClassroom( int up_to = class_size); 
+void menu();
+
+int main(){
+    menu(); 
+    cout << "Closing.. " << endl; //program closing since menu stops running
+    return 0;
+}
+
+void menu(){
+    int choice;
+    while (choice !=3 ){
+        cout << "[Menu]+---" << endl;
+        cout << "[1] Fill Classroom." << endl;
+        cout << "[2] Display Classroom." << endl;
+        cout << "[3] Close Program." << endl;
+        cout << "Choice: "; cin >> choice;
+        cout << endl << endl;
+
+        switch (choice){
+            case 1:
+                fillClassroom();
+                break;
+            case 2:
+                dispClassroom();
+                break;
+        }
+    }
+}
+
+string name_prompt( int count ) {
+    cout << "Enter First Name Of Person " << count + 1 << " : "; cin >> classA[count].fName; 
+    cout << "Enter Last Name Of Person " << count + 1 << " : "; cin >> classA[count].lName; 
+    cout << endl << endl;
+    string temp_fName = classA[count].fName;
+    string temp_fullName = temp_fName.append(classA[count].lName);
+    return temp_fullName;
+}
+
+void fillClassroom( int up_to ){ 
+    for (int i = 0 ; i < up_to ; i++ ){
+        classA[i].fullName = name_prompt(i);
+        while (classA[i].fullName.length() > name_limit) {
+            cout << "Full Name too long.." << endl;
+            classA[i].fullName = name_prompt(i);
+        }
+    }
+} 
+
+void dispClassroom( int up_to ){
+    for (int i = 0; i < up_to ; i++){
+        cout << "[" << i + 1 << "] " << "| First Name: " << classA[i].fName << " Last Name: " << classA[i].lName <<
+            " Full Name: " << classA[i].fullName << endl;
+    }
+    cout << endl << endl;
+}
