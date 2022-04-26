7e51ab87a2fbce3e1f1b4cb4fe01510df78cbb84
yassir56069 <hoossanbukshyassir@gmail.com>
2022-04-26 10:14:56 +0400

    commenting for clarity woo
    You'll see it probably--

--- classroom.h
+++ classroom.h
@@ -7,7 +7,7 @@ using namespace std;void class_menu();
 
 void class_menu();
 
-const int class_size = 5; const int name_limit = 16;
+const int class_size = 5; const int name_limit = 16; //Not recommended, but where?
 
 string name_prompt( int count );
 
