e1cad8df1d627bc0d3497b96ae663fc4aaa048b6
yassir56069 <hoossanbukshyassir@gmail.com>
2022-05-13 15:43:17 +0400

    num_sys updates, making it work

--- WordArtProject/main.cpp
+++ WordArtProject/main.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(){
     cout << "Closing.." << endl;
-    return 0
+    return 0;
 }
 
--- WordArtProject/num_sys.cpp
+++ WordArtProject/num_sys.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
+#include <chrono>
 #include "num_sys.h"
-using namespace std;
+using namespace std::chrono;
 
 /*
     _##__
@@ -65,10 +66,37 @@ using namespace std;
     _###_
 */
 
+void w_screen(int value = 0);
 
-
-int main(){
-
-    cout << "closing" << endl;
+int main() {
+    auto start = high_resolution_clock::now();
+    w_screen(0);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    int b[10]={1,2,3,4,5,6,7,8,9};
+    for(int c=0; c < 10; c++){
+        if (b[2] == 0){
+            cout << "empty";
+        } else {cout << "taken";}
+    }
+    cout << "Run Time: "<< duration.count() / 1000 << "microseconds" << endl;
     return 0;
+}
+
+
+
+void w_screen(int value){
+    int letter_num = 0;
+    string rows[1] = {
+        "01110 10001 10001 10001 01110"
+    };
+    for (int i = 0; i < 1000; i++ ){
+        letter_num = 0;
+        while(letter_num < 5){
+            if (letter_num == 0) { cout << "this"; } 
+            else if (letter_num > 1) { cout << "this"; }
+            cout << rows[letter_num] << endl;
+            letter_num ++;
+        }
+    }
 }
