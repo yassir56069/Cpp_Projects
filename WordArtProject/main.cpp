4273c54fc632bf8a8dff26c86797eeec1878aa01
yassir56069 <hoossanbukshyassir@gmail.com>
2022-05-16 18:40:45 +0400

    More OOOOP
    //I will wear you down in time 🎶

--- WordArtProject/main.cpp
+++ WordArtProject/main.cpp
@@ -7,6 +7,10 @@ class group {
         string group_name;
         int year;
     
+    protected: 
+        int num_subjects;
+        const int min_mark = 40; 
+
     public:
         group()  {
             cout << "Enter The Group Name: "; cin >> group_name;
@@ -22,15 +26,43 @@ class group {
         int getGYear(){return year;}
 };
 
-class student: public group {
+class subject: public group {
     private:
+        string subject_name;
+        int subject_avg;
+    
+    protected:
         int mark;
+
+    public:
+        subject(){
+            cout << "Enter Subject Name: "; cin >> subject_name;
+            cout << "Enter Marks In Subject: "; cin >> mark;
+            num_subjects ++;
+        }
+
+        //Setters
+        void setSname(string n){ subject_name = n;}
+        void setSavg(int a){ subject_avg = a;}
+
+        //Getters
+        string getSname(){return subject_name;}
+        int getSavg(){return subject_avg;}
+         
+        void success_rate(){
+            string result = (mark > min_mark) ? "Student Passed" : "Student Failed..";
+            cout << result << endl;
+            cout << num_subjects << endl;
+        }
+};
+
+class student: public subject {
+    private:
         string id;    
 
     public:
         student()  {
             cout << "Enter Student ID: "; cin >> id;
-            cout << "Enter Marks of student: "; cin >> mark;
         }       
 
         //Setters
@@ -48,6 +80,8 @@ class student: public group {
         }
 };
 
+
+
 double calc_avg(student, student);
 void disp_student(student);
 
@@ -56,7 +90,7 @@ int main() {
     student s_2; cout << endl;
  
     s_1.disp_student(); cout << endl; 
-    s_2.disp_student(); cout << endl; 
+    s_1.success_rate(); s_2.success_rate();
 
     cout << "Average: " << calc_avg(s_1 , s_2) << endl;
 
